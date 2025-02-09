7b70e7324fdc04552856cae59889f42ccc8ea6c3
Los Mayers <carlostmayers@gmail.com>
2025-02-09 09:24:40 -0500

    kategory day 10

--- app/page.tsx
+++ app/page.tsx
@@ -10,12 +10,13 @@ import { Analytics, track } from "@vercel/analytics/react";
 export default function WordGame() {
   const [currentAnswer, setCurrentAnswer] = useState("");
   const [currentCategoryIndex, setCurrentCategoryIndex] = useState(0);
-  const [timeRemaining, setTimeRemaining] = useState(50);
+  const [timeRemaining, setTimeRemaining] = useState(22);
   const [gameCompleted, setGameCompleted] = useState(false);
   const [userAnswers, setUserAnswers] = useState<UserAnswers>({});
   const [startTime, setStartTime] = useState<Date | null>(null);
   const [timeTaken, setTimeTaken] = useState<number | null>(null);
-  const startingLetter = "Y";
+  const startingLetter = "F";
+  // 2-08-2025 was F
   // 2-08-2025 was Y
   // 2-07-2025 was C
   // 2-06-2025 was J
